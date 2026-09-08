e29163b47d460f8719662e7bf405e12e9b4852e6
rampmail29 <113305123+rampmail29@users.noreply.github.com>
2026-09-08 16:18:01 -0500

    fix: /backup folder deleted

--- backup/environment.js
+++
@@ -1,9 +0,0 @@
-/**
- * environment.js
- * Responsabilidad (sección 13): centralizar configuración del entorno.
- * NO escribir URLs de backend directamente en componentes.
- */
-export const ENV = {
-  //ejemplo de variable de entorno: si no se tiene un proceso de despliegue, entonces toma el enlace de localhost
-  API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
-};
